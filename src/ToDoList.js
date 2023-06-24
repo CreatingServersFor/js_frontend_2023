356427d010f09159e55cad5eba196a6af0a08226
CreatingServersFor <dotnetframeworker@rambler.ru>
2023-06-24 13:20:20 +0300

    redact classNameName -> className in ToDoList.js

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -6,7 +6,7 @@ import ToDoTask from './ToDoTask';
 class ToDoList extends React.Component {
   render(){
 	return (
-      <div classNameName="List">
+      <div className="List">
 		<div className="card-hover-shadow-2x mb-3 card">
         <div className="card-header-tab card-header">
           <div className="card-header-title font-size-lg text-capitalize font-weight-normal">
